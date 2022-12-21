4eed132ab7df4d4ef17bf862c46561c3a313c1b7
Andrii Derhach <thequasar7@gmail.com>
2022-12-22 00:41:45 +0200

    Move target fn to lib.rs

--- src/hill_climb.rs
+++ src/hill_climb.rs
@@ -1,17 +1,5 @@
+use super::target_fn;
 use crate::binary_vec::BinaryVec;
-
-pub fn target_fn(bin_vec: &BinaryVec, values: &Vec<f64>) -> f64 {
-    let nums = bin_vec.as_nums();
-    let sum1 = nums
-        .iter()
-        .enumerate()
-        .map(|(i, e)| *values.get(i).unwrap() * *e as f64)
-        .sum::<f64>();
-
-    let sum2 = values.iter().sum::<f64>() / 2.0;
-
-    (sum1 - sum2).abs()
-}
 
 pub fn hill_climb_min(values: &Vec<f64>) -> BinaryVec {
     let mut x = BinaryVec::random(values.len());
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
+use binary_vec::BinaryVec;
+
 pub mod binary_vec;
 pub mod hill_climb;
+
+pub fn target_fn(bin_vec: &BinaryVec, values: &Vec<f64>) -> f64 {
+    let nums = bin_vec.as_nums();
+    let sum1 = nums
+        .iter()
+        .enumerate()
+        .map(|(i, e)| *values.get(i).unwrap() * *e as f64)
+        .sum::<f64>();
+
+    let sum2 = values.iter().sum::<f64>() / 2.0;
+
+    (sum1 - sum2).abs()
+}
